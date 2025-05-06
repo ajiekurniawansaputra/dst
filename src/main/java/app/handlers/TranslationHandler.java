90f84ca6eaf93630836618b33a15f1fa65941a89
Ajie <ajie@rewash.id>
2025-05-06 15:47:36 +0700

    1.1.1 Endpoint Translate (Mock)

--- src/main/java/app/handlers/TranslationHandler.java
+++ src/main/java/app/handlers/TranslationHandler.java
@@ -1,6 +1,13 @@
 package app.handlers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import ratpack.exec.Promise;
 import ratpack.handling.Chain;
+import ratpack.jackson.Jackson;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class TranslationHandler {
     public static void addRoutes(Chain chain) throws Exception {
@@ -9,9 +16,23 @@ public class TranslationHandler {
                 ctx.byMethod(m -> m
                     // Translate text to one country
                     .get(() -> {
-                        ctx.render("Translate text to one country");
+                        ctx.getRequest().getBody().then(body -> {
+                            Map<String, Object> bodymap = new ObjectMapper().readValue(body.getText(), HashMap.class);
+                            if (bodymap.get("text") == null || bodymap.get("language") == null) {
+                                HashMap<String, Object> errorResponse = new HashMap<>();
+                                errorResponse.put("status", "error");
+                                errorResponse.put("message", "400 Bad Request : Text and Language is required");
+                                ctx.getResponse().status(400);
+                                ctx.render(Jackson.json(errorResponse));
+                            }
+                            HashMap<String, Object> stringMap = new HashMap<>();
+                            stringMap.put("text", bodymap.get("text"));
+                            stringMap.put("language", bodymap.get("language"));
+                            stringMap.put("translatedText", "Text ini di dalam bahasa yang kamu pahami");
+                            ctx.getResponse().status(200);
+                            ctx.render(Jackson.json(stringMap));
+                        });
                     })
-                )
-            );
+            ));
     }
 }
